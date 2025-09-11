72c107ffbbcd50834a1b39bf1c2949e33d553f5b
nomamonx <nomanmontage@gmail.com>
2025-09-11 17:09:48 +0800

    Update hwid1.lua

--- hwid1.lua
+++ hwid1.lua
@@ -1,5 +1,5 @@
 return {
-    ["2dd5cdbc-41c0-4b90-933d-d056fee68e8"] = true,
+    ["2dd5cdbc-41c0-4b90-933d-d056fee68e87"] = true,
     ["f59e51d7-c465-4f24-997a-68fcbe74b66f"] = true,
     ["cfb0a"] = true,
     ["cfb0af"] = true,
