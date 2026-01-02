8d62a7481f7fe351eeaaa5e325de3d6d3726e7d9
nomamonx <nomanmontage@gmail.com>
2026-01-02 21:22:52 +0800

    Add new HWID entry to hwid1.lua

--- hwid1.lua
+++ hwid1.lua
@@ -4,4 +4,5 @@ return {
     ["79ec1c37-dbaf-4eba-a29a-72b6c2792d15"] = true,
     ["78e05f54-5614-4f38-b154-c98a9c82fcbd"] = true,
     ["DF52B74A-B972-443E-9880-F2660022E3D9"] = true,
+    ["5c033ed4-9215-48dc-917e-d962991a8398"] = true,
 }
