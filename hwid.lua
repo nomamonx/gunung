1a41a901a98ac4b745f0773a098b882063b264f4
nomamonx <nomanmontage@gmail.com>
2026-01-02 21:22:31 +0800

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -3,4 +3,5 @@ return {
     ["f8c51e5b-a874-4d44-80eb-babcd2df7402"] = true,
      ["78e05f54-5614-4f38-b154-c98a9c82fcbd"] = true,
     ["DF52B74A-B972-443E-9880-F2660022E3D9"] = true,
+    ["5c033ed4-9215-48dc-917e-d962991a8398"] = true,
 }
