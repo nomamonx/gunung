952e9ea7c654344a9d15b8c95bf6813ea144c311
nomamonx <nomanmontage@gmail.com>
2026-01-04 05:10:44 +0800

    Replace HWID with a new identifier

--- hwid1.lua
+++ hwid1.lua
@@ -1,5 +1,5 @@
 return {
-    ["2dd5cdbc-41c0-4b90-933d-d056fee68e87"] = true,
+    ["c6d7cc5b-59f6-4b2d-a441-a71cef25e341"] = true,
     ["f59e51d7-c465-4f24-997a-68fcbe74b66f"] = true,
     ["79ec1c37-dbaf-4eba-a29a-72b6c2792d15"] = true,
     ["78e05f54-5614-4f38-b154-c98a9c82fcbd"] = true,
