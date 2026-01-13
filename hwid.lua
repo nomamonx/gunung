35b272f606c23694b249f8ac211658a02e539032
nomamonx <nomanmontage@gmail.com>
2026-01-13 21:16:51 +0800

    Replace UUID in hwid.lua with a new entry

--- hwid.lua
+++ hwid.lua
@@ -1,6 +1,6 @@
 return {
     ["c6d7cc5b-59f6-4b2d-a441-a71cef25e341"] = true,
-    ["83abeb69-890e-43ee-9c95-a726f09030c8"] = true,
+    ["e85459c3-f6b7-496c-b544-e567331e93e9"] = true,
      ["78e05f54-5614-4f38-b154-c98a9c82fcbd"] = true,
     ["DF52B74A-B972-443E-9880-F2660022E3D9"] = true,
     ["5c033ed4-9215-48dc-917e-d962991a8398"] = true,
